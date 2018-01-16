e25611f649e36bcfbfa2392d2e18c5213e5b7ea7
MohamedOsamaMohammed <mohamedosama914@users.noreply.github.com>
2018-01-16 10:17:57 +0200

    MohammedOsamaMohammed
    MohammedOsamaMohammed

---
+++ Visual Studio - Kugou Music Download Tool/Kugou/Kugou/KugouResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kugou
+{
+    class KugouResult
+    {
+        public string filename { get; set; }
+        public string sqhash { get; set; }
+        public string key { get; set; }
+    }
+}
