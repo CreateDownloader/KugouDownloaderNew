78454770210e9380178b8eda56f5fdcc1dc6b721
Mohammed Osama <mohamedosama914@users.noreply.github.com>
2018-01-26 15:48:04 +0200

    Create Old.Kugou.index.html.cs

---
+++ Old.Kugou.index.html.cs
@@ -0,0 +1,11 @@
+<cs>
+Now
+</cs>
+<url>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">    
+  <link href="https://www.googleplaymusicdesktopplayer.com/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="https://www.googleplaymusicdesktopplayer.com/css/style.css?2" type="text/css" rel="stylesheet" media="screen,projection"/>
+    </url>
